f951983883d00a8648268ef9735707cc7bc6a7bd
Daniel Silva <daniel.jqh@outlook.com>
2021-08-25 00:56:25 -0300

    Alteração sessão serviços

--- application/views/home.php
+++ application/views/home.php
@@ -4,134 +4,147 @@
     <div class="row row-hero mx-auto">
       <div class="col-12 col-lg-6 pt-4 pt-lg-0 my-auto">
         <div class="container position-relative text-left">
-          <h1>Inovação movida por <span style="color: #1111;">resultados</span> </h1>
+          <h1>Inovação movida por <span style="color: #2b89bb;">resultados</span> </h1>
           <h2>Dê o próximo passo para o sucesso do seu projeto</h2>
           <div class="btn-hero">
             <a href="#servicos" class="btn-get-started scrollto">Conhecer a Kronus</a>
           </div>
         </div>
       </div>
-      <div class="col-lg-6 gif-hero">
-        <img src="<?php echo base_url(); ?>public/assets/img/gif/web-design.gif" class="img-fluid" alt="Gif tela inicial Kronus"></img>
-      </div>
     </div>
   </div>
-  <svg id="svg-inicio" data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1417.98 194.15">
-    <path class="cls-1" d="M-273.53,318Q-277,187.3-280.66,56.61q958.47,34.22,1917.06,68.3c2,32.4,3.93,64.92,5.78,97.33Q684.45,270.27-273.53,318Z" transform="translate(0)" />
-  </svg>
 </section>
-<!-- Serviços -->
-<main id="main">
-  <section id="servicos" class="servicos">
-    <div class="container">
-      <div class="section-title">
-        <h2>Soluções</h2>
-      </div>
-      <div class="row slider">
-        <div class="col-md-12">
-          <div class="box institucional">
-            <h3>Criação de Sites</h3>
-            <h1><i class='bx bx-pointer'></i></h1>
-            <p class="text-justify">
+
+<section class="servicos" style="width: 100%;">
+  <div class="container card-styles">
+    <div class="row slider" id="cards">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="icon-wrap px-4 pt-4">
+            <div class="icon d-flex justify-content-center align-items-center card-icon rounded-circle">
+              <span class="ion-logo-ionic text-light"></span>
+            </div>
+          </div>
+          <div class="card-body pb-5 px-4">
+            <h5 class="card-title">Criação de Sites</h5>
+            <p class="card-text">
               O digital é o futuro, aliás, é o nosso presente saia do convencional destaque-se da concorrência,
               tenha um site feito para trazer clientes e credibilidade para sua empresa integralmente, com segurança e rapidez
             </p>
+            <a href="#" class="btn card-btn">Saiba mais</a>
           </div>
         </div>
-        <div class="col-md-12">
-          <div class="box featured">
-            <h3>E-commerce</h3>
-            <h1><i class='bx bx-cart-alt'></i></h1>
-            <p class="text-justify">
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="icon-wrap px-4 pt-4">
+            <div class="icon d-flex justify-content-center align-items-center card-icon rounded-circle">
+              <span class="ion-logo-ionic text-light"></span>
+            </div>
+          </div>
+          <div class="card-body pb-5 px-4">
+            <h5 class="card-title">E-commerce</h5>
+            <p class="card-text">
               Aqui nós criamos uma loja online sob medida para sua empresa, focada em vender o seu produto seja ele digital ou físico,
-              dê a seus clientes uma experiência única, com um design intuitivo, focado em gerar venda
+              dê a seus clientes uma experiência única, com um design intuitivo, focado em gerar venda.
             </p>
+            <a href="#" class="btn card-btn">Saiba mais</a>
           </div>
         </div>
-        <div class="col-md-12">
-          <div class="box landing-page">
-            <h3>Google Adwords</h3>
-            <h1><i class='bx bx-left-indent'></i></h1>
-            <p class="text-justify">
-              Apareca nos resultados das buscas do google para quem está realmente interessado no seu produto e serviço, aumentando o resultado e otimizando o investimento.
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="icon-wrap px-4 pt-4">
+            <div class="icon d-flex justify-content-center align-items-center card-icon rounded-circle">
+              <span class="ion-logo-ionic text-light"></span>
+            </div>
+          </div>
+          <div class="card-body pb-5 px-4">
+            <h5 class="card-title">Gestão de Redes Sociais</h5>
+            <p class="card-text">
+              Esteja nas redes sociais para gerar resultados para sua empresa,
+              passando valor e se conectando com seus clientes, por meio de estratégias de marketing e criação de conteúdo
             </p>
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="box portfolio">
-            <h3>Gestão de Redes Sociais</h3>
-            <h1><i class='bx bxl-instagram-alt'></i></h1>
-            <p class="text-justify mx-auto">
-              Esteja nas redes sociais para gerar resultados para sua empresa, passando valor e se conectando com seus clientes, por meio de estratégias de marketing e criação de conteúdo
-            </p>
-            <!--  <div class="btn-wrap">
-              <a href="#" class="btn-buy">Orçamento Personalizado</a>
-            </div> -->
+            <a href="#"  class="btn card-btn">Saiba mais</a>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-  <!-- ======= About servicos ======= -->
-  <section id="about-servicos" class="about-servicos">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <img src="<?php echo base_url(); ?>public/assets/img/about-bg.png" class="img-fluid" alt="">
-        </div>
-        <div class="col-lg-6 pt-4 pt-lg-0">
-          <h3>Aqui seus objetivos também são nossos</h3>
-          <h4>
-            Nosso maior foco é fazer o seu negócio decolar, obtendo resultados em alta performance
-          </h4>
-
-          
+      <div class="col-md-4">
+        <div class="card">
+          <div class="icon-wrap px-4 pt-4">
+            <div class="icon d-flex justify-content-center align-items-center card-icon rounded-circle">
+              <span class="ion-logo-ionic text-light"></span>
+            </div>
+          </div>
+          <div class="card-body pb-5 px-4">
+            <h5 class="card-title">Card title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content. A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <a href="#" class="btn card-btn">Saiba mais</a>
           </div>
         </div>
       </div>
     </div>
-  </section>
-  <!-- ======= Resumo ======= -->
-  <section id="resumo" class="resumo">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <img src="<?php echo base_url(); ?>public/assets/img/resumebg.png" class="img-fluid" alt="">
-        </div>
-        <div class="col-lg-6 pt-4 pt-lg-0 my-auto">
-          <h3>Nós nascemos da necessidade de <span>destacar empresas</span> como a sua através da inovação!</h3>
-          <p class="resume-desc">Tornando ela visível em todos os meios digitais, seja em sites, redes socias ou buscas do google,
-            esse é nosso combustível.</p>
-          <p class="resume-cta">
-            Conseguir se destacar é somente para as poucas pessoas que tem um desejo insaciável de
-            estar a sempre a frente no mercado, se você chegou até aqui <span>você pertence a esse pequeno grupo!</span>
-          </p>
-        </div>
+  </div>
+</section>
+<section id="about-servicos" class="about-servicos">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <img src="<?php echo base_url(); ?>public/assets/img/about-bg.png" class="img-fluid" alt="">
+      </div>
+      <div class="col-lg-6 pt-4 pt-lg-0">
+        <h3>Aqui seus objetivos também são nossos</h3>
+        <h4>
+          Nosso maior foco é fazer o seu negócio decolar, obtendo resultados em alta performance
+        </h4>
+
 
       </div>
     </div>
-  </section>
-  <!-- ======= Fale conosco ======= -->
-  <section id="cta" class="cta">
-    <div class="container">
-
-      <div class="text-center">
-        <h3>Podemos sim tornar o seu objetivo alcançável</h3>
-        <h3>Mas estamos aqui para te fazer superá-lo!</h3>
-        <p></p>
-        <a class="cta-btn" href="#">Vamos decolar!</a>
+  </div>
+  </div>
+</section>
+<!-- ======= Resumo ======= -->
+<section id="resumo" class="resumo">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <img src="<?php echo base_url(); ?>public/assets/img/resumebg.png" class="img-fluid" alt="">
       </div>
-    </div>
-  </section>
-  <!-- ======= Resumo do blog ======= -->
-  <section id="resumo-blog">
-    <div class="container text-center">
-      <h3>Esteja a frente sempre</h3>
-      <p>A Kronus ajuda a sua empresa a chegar no topo. </p>
-      <p>
-        Se você chegou até aqui, é por que está cansado de estar no estafo estacionário, com seu negócio parado
+      <div class="col-lg-6 pt-4 pt-lg-0 my-auto">
+        <h3>Nós nascemos da necessidade de <span>destacar empresas</span> como a sua através da inovação!</h3>
+        <p class="resume-desc">Tornando ela visível em todos os meios digitais, seja em sites, redes socias ou buscas do google,
+          esse é nosso combustível.</p>
+        <p class="resume-cta">
+          Conseguir se destacar é somente para as poucas pessoas que tem um desejo insaciável de
+          estar a sempre a frente no mercado, se você chegou até aqui <span>você pertence a esse pequeno grupo!</span>
+        </p>
+      </div>
 
-      </p>
     </div>
-  </section>
+  </div>
+</section>
+<!-- ======= Fale conosco ======= -->
+<section id="cta" class="cta">
+  <div class="container">
+
+    <div class="text-center">
+      <h3>Podemos sim tornar o seu objetivo alcançável</h3>
+      <h3>Mas estamos aqui para te fazer superá-lo!</h3>
+      <p></p>
+      <a class="cta-btn" href="#">Vamos decolar!</a>
+    </div>
+  </div>
+</section>
+<!-- ======= Resumo do blog ======= -->
+<section id="resumo-blog">
+  <div class="container text-center">
+    <h3>Esteja a frente sempre</h3>
+    <p>A Kronus ajuda a sua empresa a chegar no topo. </p>
+    <p>
+      Se você chegou até aqui, é por que está cansado de estar no estafo estacionário, com seu negócio parado
+
+    </p>
+  </div>
+</section>
 </main>
--- application/views/sobre.php
+++ application/views/sobre.php
@@ -10,6 +10,17 @@
      <div class="row">
        <div class="col-lg-6 pt-4 pt-lg-0">
          <h1>Por que Kronus?</h1>
+         <p>
+           Na mitologia grega, Chronos (Cronos) é o Deus do Tempo.
+           Fizemos uma adaptação transformando em Kronus, para agregar à
+           fonética e à exclusividade da marca.
+           Esse nome se alinha perfeitamente com o conceito da empresa no
+           sentido de estar sempre considerando as tendências de mercado e as
+           novas tecnologias, para assim, estar a frente nos lançamentos e atender
+           seus clientes com a melhor e mais avançada tecnologia disponível.
+           Por isso, com o tempo, o intuito é fazer a palavra Kronus ser sinônimo de
+           responsabilidade, expertise e inovação.
+         </p>
        </div>
        <div class="col-lg-6 pt-4 pt-lg-0">
          <h3>Aqui seus objetivos também são nossos</h3>
@@ -33,9 +44,48 @@
      </div>
    </div>
  </section>
+ <section id="sobre" class="sobre mt-3">
+   <div class="container">
+     <div class="row">  
+       <div class="col-lg-6 pt-4 pt-lg-0">
+         <h3>Aqui seus objetivos também são nossos</h3>
+         <h4>
+           Nosso maior foco é fazer o seu negócio decolar, obtendo resultados em alta performance
+         </h4>
+
+         <div class="row">
+           <div class="col-md-6">
+             <i class="bx bx-receipt"></i>
+             <h4>Esteja presente</h4>
+             <p>Tenha o seu negócio aberto 24h, acessível de qualquer lugar do mundo.</p>
+           </div>
+           <div class="col-md-6">
+             <i class="bx bx-cube-alt"></i>
+             <h4>Seja encontrado</h4>
+             <p>Esteja no topo nas pesquisas do Google</p>
+           </div>
+         </div>
+       </div>
+       <div class="col-lg-6 pt-4 pt-lg-0">
+         <h1>Quem somos nós?</h1>
+         <p>
+           Na mitologia grega, Chronos (Cronos) é o Deus do Tempo.
+           Fizemos uma adaptação transformando em Kronus, para agregar à
+           fonética e à exclusividade da marca.
+           Esse nome se alinha perfeitamente com o conceito da empresa no
+           sentido de estar sempre considerando as tendências de mercado e as
+           novas tecnologias, para assim, estar a frente nos lançamentos e atender
+           seus clientes com a melhor e mais avançada tecnologia disponível.
+           Por isso, com o tempo, o intuito é fazer a palavra Kronus ser sinônimo de
+           responsabilidade, expertise e inovação.
+         </p>
+       </div>
+     </div>
+   </div>
+ </section>
  <!-- ======= Home Sobre ======= -->
  <section>
    <div class="container">
-    
+
    </div>
  </section>
